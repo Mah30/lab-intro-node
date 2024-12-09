628b71c7a57e6081e78dbe006267b52bf9937d0b
Mariah <alinemariah@hotmail.com>
2024-12-09 19:50:22 +0100

    lab intro node js

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    
+      this.items = [];
+      this.length = 0
 
-  add(item) {}
+    
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  max() {}
 
-  min() {}
+  get(pos) {
 
-  sum() {}
+    if (pos < 0 || pos >= this.items.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  avg() {}
+
+  // return the highest value of the array
+  max() {
+    if (this.items.length === 0){
+      throw new Error ("EmptySortedList");
+    }
+    return Math.max(...this.items);
+    
+// or return this.items[this.items.length-1];
+
+  }
+
+  min() {
+    if (this.items.length === 0){
+      throw new Error ("Empty SortedList");
+
+    }
+    return this.items[0];
+  }
+
+  sum() {
+
+    return this.items.reduce((sum, next) => 
+     sum + next, 0);
+  }
+
+  avg() {
+    if (this.items-this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    
+     return this.items.reduce((sum, next) => 
+     sum + next, 0)/this.items.length;
+  }
 }
 
 module.exports = SortedList;
